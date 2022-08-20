fee813befe7286b94bd49cd48debcdc992a06110
larsomic <michael.j.larson@wsu.edu>
2022-08-19 22:20:36 -0700

    Adding blank MERN app

---
+++ server/models/projects.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const projectSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+});
+
+const Project = mongoose.model('Projct', projectSchema);
+
+export default Project;
